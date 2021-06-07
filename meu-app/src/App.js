b0d12a90c3a1ec75d669516870f8f7468f1ed091
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-07 13:53:45 -0400

    Criando meu primeiro componente e atrubuindo as props

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite o arquivo <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header name="Wesley" links={["Sobre", "Comprar", "Contato"]}></Header>
   );
 }
 
