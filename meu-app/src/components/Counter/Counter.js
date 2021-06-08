f7596b13ae640824e8af9a66c4a18f1cb23642e8
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-08 13:00:53 -0400

    Métodos de ciclo de vida

--- meu-app/src/components/Counter/Counter.js
+++ meu-app/src/components/Counter/Counter.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
 
-class Counter extends React.Component{
+class Counter extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state = {count: this.props.count}
+        this.state = {count: 0}
         this.add = this.add.bind(this);
     }
 
-    add(){
-         this.setState((state)=>{return {count: state.count + 1}})
+    add() {
+        this.setState((state) => { return { count: state.count + 1 } }, () => { 
+            console.log(this.state) 
+            localStorage.setItem("state", JSON.stringify(this.state))
+        });
     }
 
-    render(){
-        return(<div>
-                    <h1>Counter: {this.state.count}</h1>
-                    <button onClick={this.add} >add</button>
-                </div>)
+    componentDidMount(){
+        this.setState(JSON.parse(localStorage.getItem("state")));
+    }
+    
+    render() {
+        return (<div>
+            <h1>Counter: {this.state.count}</h1>
+            <button onClick={this.add} >add</button>
+        </div>)
     }
 }
 
