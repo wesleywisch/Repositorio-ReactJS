d79640655558ff994d9ded5b4ea9181436597f8d
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-08 10:25:54 -0400

    Criando um componente com class

--- meu-app/src/components/Header/header.js
+++ meu-app/src/components/Header/header.js
@@ -2,13 +2,36 @@ import React from 'react';
 import './App.css';
 import Menu from '../Menu/Menu'
 
-function Header(props) {
-    return (
-        <header className="header">
-            <h1>{props.name}</h1>
-            <Menu links={props.links}></Menu>
-        </header>
-    )
+// function Header(props) {
+//     return (
+//         <header className="header">
+//             <h1>{props.name}</h1>
+//             <Menu links={props.links}></Menu>
+//         </header>
+//     )
+// }
+
+class Header extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+
+
+    render() {
+        return (
+            <header className="header">
+                <h1>{this.props.name}</h1>
+                <Menu links={this.props.links}></Menu>
+            </header>
+        )
+    }
+
 }
+
+
+
+
 
 export default Header;
