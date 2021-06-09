076a228712f03a090887c00952c13b63efbb0770
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-09 08:29:13 -0400

    Hook effect equivalente ao Unmount

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from './components/Header/Header'
 import Counter from './components/Counter/Counter';
 
 
 function App() {
-  return (
-    <div>
-      <Header name="Wesley" links={["Sobre", "Comprar", "Contato", "Login"]}></Header>
-      <Counter count={0}></Counter>
-    </div>
-  );
+
+  const [show, setShow] = useState(true);
+  let time = 0;
+
+  useEffect(()=>{
+
+    let timer = setInterval( () => {
+      time++;
+      console.log(time);
+  
+      if (time > 5) {
+        clearInterval(timer);
+        setShow(false)
+      }
+    }, 1000);
+
+  })
+
+  if (show) {
+    return (
+      <div>
+        <Header name="Wesley" links={["Sobre", "Comprar", "Contato", "Login"]}></Header>
+        <Counter count={3}></Counter>
+      </div>
+    );
+
+  } else {
+
+    return (
+      <div>
+        <Header name="Wesley" links={["Sobre", "Comprar", "Contato", "Login"]}></Header>
+        Sem contador!
+      </div>
+    );
+  }
 }
 
 export default App;
