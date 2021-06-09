ae9fe26dbd2e0cbff3d3c3b3f7ac2a6eff664ba5
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-09 07:48:17 -0400

    Criando um hook estado

--- meu-app/src/components/Counter/Counter.js
+++ meu-app/src/components/Counter/Counter.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-class Counter extends React.Component {
 
-    constructor(props) {
-        super(props);
+function Counter(props){
+    // Hook estado
+    const [count, setCount] = useState(props.count);
 
-        this.state = {count: 0}
-        this.add = this.add.bind(this);
+    function add(){
+        setCount(count + 1)
     }
 
-    add() {
-        this.setState((state) => { return { count: state.count + 1 } }, () => { 
-            console.log(this.state) 
-            localStorage.setItem("state", JSON.stringify(this.state))
-        });
-    }
-
-    componentDidMount(){
-        this.setState(JSON.parse(localStorage.getItem("state")));
-    }
-    
-    render() {
-        return (<div>
-            <h1>Counter: {this.state.count}</h1>
-            <button onClick={this.add} >add</button>
-        </div>)
-    }
+    return(
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={add}>add</button>
+        </div>
+    )
 }
+
+
+
+// class Counter extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {count: 0}
+//         this.add = this.add.bind(this);
+//     }
+
+//     add() {
+//         this.setState((state) => { return { count: state.count + 1 } }, () => { 
+//             console.log(this.state) 
+//             localStorage.setItem("state", JSON.stringify(this.state))
+//         });
+//     }
+
+//     componentDidMount(){
+//         this.setState(JSON.parse(localStorage.getItem("state")));
+//     }
+    
+//     render() {
+//         return (<div>
+//             <h1>Counter: {this.state.count}</h1>
+//             <button onClick={this.add} >add</button>
+//         </div>)
+//     }
+// }
 
 export default Counter;
