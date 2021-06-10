ace9e655bdf8d22e99a7c4b90d337ee35e5b51ed
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-10 13:22:23 -0400

    Adicionando um item na lista

--- lista-de-tarefas/src/App.js
+++ lista-de-tarefas/src/App.js
@@ -3,23 +3,38 @@ import './Todo.css';
 
 function TodoList(){
 
+    // estado do input
     const [text, setText] = useState("");
 
+    // estado da lista
+    const [items, setItems] = useState([]);
+
+    // função para pegar o valor do input
     function handleChenge(event){
         let textInput = event.target.value;
         setText(textInput);
+    }
+
+    // função para adicionar um item na lista
+    function addItem(event){
+        event.preventDefault();
+        if(text){
+            setItems([...items, text]);
+            setText("");
+        }
+
     }
 
     return(<div className="container">
                 <h1>TodoList</h1>
 
                 <form>
-                    <input onChange={handleChenge} type="text"></input>
-                    <button>Adicionar</button>
+                    <input onChange={handleChenge} type="text" value={text}></input>
+                    <button onClick={addItem}>Adicionar</button>
                 </form>
 
                 <ul>
-                    <li>{text}</li>
+                    {items.map(item=><li>{item}</li>)}
                 </ul>
             </div>)
 
