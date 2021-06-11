966f5bc8ba7ab497ffd77f42c966eea602599641
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-11 14:02:31 -0400

    Salvando a lista no localstorage

--- lista-de-tarefas/src/App.js
+++ lista-de-tarefas/src/App.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import List from './components/Lista/List';
 import TodoForm from './components/Formulario/TodoForm';
 import Item from './components/Estrutura/Item';
 import './Todo.css';
 
+const SAVED_ITEMS = "savedItems"
+
 function TodoList(){
 
     // estado da lista
     const [items, setItems] = useState([]);
 
+    useEffect(()=>{
+        let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS));
+
+        if(savedItems){
+            setItems(savedItems);
+        }
+    }, []) // esse e para apenas retornar a lista, ele retorna so quando a página for carregada ou seja so executada apenas 1 vez
+
+    useEffect(()=>{
+        localStorage.setItem(SAVED_ITEMS, JSON.stringify(items));
+    }, [items]) // esse serve para observar o estado ITEMS e quando for modificado ele salva no localstorage.
+
     // função para adicionar um item na lista
     function onAddItem(text){
 
         let it = new Item(text);
+
+        it.id = items.length
 
         setItems([...items, it])
     }
