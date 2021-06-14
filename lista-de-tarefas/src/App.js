341ca0a59230d4901aba787d555f9a040b4a5739
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-14 10:25:25 -0400

    Redux - persistindo os dados

--- lista-de-tarefas/src/App.js
+++ lista-de-tarefas/src/App.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 
 import List from './components/Lista/List';
 import TodoForm from './components/Formulario/TodoForm';
-import Item from './components/Estrutura/Item';
 import Modal from './components/Modal/Modal';
 
 import { createStore } from 'redux';
@@ -12,66 +11,34 @@ import { Provider } from 'react-redux';
 import listReducer from './reducers/listReducer';
 
 
-
-const store = createStore(listReducer);
-
 const SAVED_ITEMS = "savedItems"
+
+
+function persistState(state){
+    localStorage.setItem(SAVED_ITEMS, JSON.stringify(state));
+}
+
+function loadState(){
+    const actualState = localStorage.getItem(SAVED_ITEMS);
+
+    if(actualState){
+        return JSON.parse(actualState);
+    }else{
+        return [];
+    }
+}
+
+
+const store = createStore(listReducer, loadState() );
+
+store.subscribe( () => {
+    persistState(store.getState() )
+})
+
 
 function TodoList(){
 
     const [showModal, setShowModal] = useState(false);
-
-    // estado da lista
-    // const [items, setItems] = useState([]);
-
-    // useEffect(()=>{
-    //     let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS));
-
-    //     if(savedItems){
-    //         setItems(savedItems);
-    //     }
-    // }, []) // esse e para apenas retornar a lista, ele retorna so quando a página for carregada ou seja so executada apenas 1 vez
-
-    // useEffect(()=>{
-    //     localStorage.setItem(SAVED_ITEMS, JSON.stringify(items));
-    // }, [items]) // esse serve para observar o estado ITEMS e quando for modificado ele salva no localstorage.
-
-
-
-
-    // // função para adicionar um item na lista
-    // function onAddItem(text){
-
-    //     let it = new Item(text);
-
-    //     it.id = items.length
-
-    //     setItems([...items, it]);
-    //     onHideModal();
-    // }
-
-    // //função para deletar um item da lista
-    // function onItemDelete(item){
-
-    //     let filteredItems = items.filter(it => it.id !== item.id);
-
-    //     setItems(filteredItems);
-    // }
-
-    // // função para verificar se a tarefa já foi completada
-    // function onDone(item){
-
-    //     let updatedItems = items.map(it =>{
-    //         if(it.id === item.id){
-    //             it.done = !it.done;
-    //         }
-    //         return it;
-    //     })
-
-    //     setItems(updatedItems)
-
-    // }
-
 
 
     function onHideModal(event){
--- lista-de-tarefas/src/reducers/listReducer.js
+++ lista-de-tarefas/src/reducers/listReducer.js
@@ -1,17 +1,17 @@
 
 
-export default function (state = [], action){
+export default function listReducer(state = [], action){
 
     switch(action.type){
         case "ADD_ITEM": 
             return [...state, action.payload]
 
         case "DELETE_ITEM":
-            return state.filter(item => item.id != action.payload )
+            return state.filter(item => item.id !== action.payload )
 
         case "CHANGE_DONE":
             return state.map(item => {
-                if(item.id == action.payload){
+                if(item.id === action.payload){
                     item.done = !item.done
                 }
                 return item;
