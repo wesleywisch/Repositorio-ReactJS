8fff271bc0b0e4342f516b672ed1ecadf5e37dd1
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-15 08:44:50 -0400

    Criando seu próprio Hook (custom hook)

--- react-routers/src/components/Aula/Aula.js
+++ react-routers/src/components/Aula/Aula.js
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router';
 
 import Data from '../../Data';
 
+
+function useAula(){
+    const id = useParams().id
+    return Data.filter(aula => aula.id == id )[0]
+}
+
+
+
 export default function Aula(props) {
-    const params = useParams();
+    const aula = useAula();
 
-    const [aula, setAula] = useState('');
-
-    useEffect( () =>{
-        setAula(Data.filter(aula => aula.id == params.id )[0]);
-    }, [params]);
     
     return(
         <div className="page">
