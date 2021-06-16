82e780467b277e00ff6227d2f3ad5e213d758762
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-16 10:13:13 -0400

    Projeto que integra o node + react

---
+++ vídeos/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const exp = require('constants');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const videos = [
+    "sUm3mvZii6Y",
+    "DvwMVIJbOJk",
+    "Zu073TPW3E8",
+    "mvMJl_0oQ_8",
+    "mvMJl_0oQ_8",
+]
+
+app.get('/api/videos', (req, res) =>{
+    res.send(videos);
+})
+
+app.use(express.static( path.join(__dirname, 'front/build')) )
+
+app.get('*', (req, res) =>{
+    res.sendFile( path.join(__dirname, 'front/build/index.html', function(error){
+        if(error){
+            res.status(500).send(error);
+        }
+    }))
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Servidor rodando!")
+})
